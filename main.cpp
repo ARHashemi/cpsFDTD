08af6530c00ce5b1eb5f511dbdf9b4f2fd02b67c
ARHashemi <arezahashemi@gmail.com>
2022-08-01 16:25:52 +0430

    add licence information

--- main.cpp
+++ main.cpp
@@ -1,3 +1,19 @@
+//    This is an example main file for the arhFDTD package
+//    Copyright (C) <2022>  <AliReza Hashemi>
+
+//    This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU Affero General Public License as
+//    published by the Free Software Foundation, either version 3 of the
+//    License, or (at your option) any later version.
+
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU Affero General Public License for more details.
+
+//    You should have received a copy of the GNU Affero General Public License
+//    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 #include "stdafx.h"
 #include <stdio.h>
 #include <math.h>
@@ -45,8 +61,8 @@ using namespace cimg_library;
 //#include "IOimage.h"
 #include "Ports.h"
 #include "TF_SF.h"
-#include "DL_model.h"
-#include "Fluorescence.h"
+//#include "DL_model.h"
+//#include "Fluorescence.h"
 
 const char material_file[20] = "material.txt";
 const char mesh_file[30] = "structure.txt";
